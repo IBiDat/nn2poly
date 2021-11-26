9339d68255ba96a6e7e1c771cbfa6b43b476b146
moralapablo <moralapablo@gmail.com>
2021-11-26 03:24:17 +0100

    reverted changes in .onload?

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 
 .onLoad <- function(libname, pkgname) {
-  reticulate::source_python(system.file("python/generate_partitions.py", package = "nn2pr"))
+  reticulate::source_python(system.file("python/generate_partitions.py", package = "nn2pr"),  envir = globalenv())
 }
