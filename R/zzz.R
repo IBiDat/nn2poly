9b028b5a161ca0f4de3d29465d088773c19e985a
moralapablo <moralapablo@gmail.com>
2022-01-23 13:00:15 +0100

    added the old version of generate partitions to the .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   reticulate::source_python(system.file("python/generate_partitions.py", package = "nn2poly"), envir = globalenv())
+  reticulate::source_python(system.file("python/generate_partitions_old.py", package = "nn2poly"), envir = globalenv())
+
 }
