b3a9a30bf4b5be2969a979946b9f05f03fbd7f7e
Iñaki Úcar <iucar@fedoraproject.org>
2022-06-17 13:10:11 +0200

    small fix + add fake vignettes to silence warning

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -5,4 +5,5 @@ knitr::knit("vignettes/source/nn2poly-03-data.Rmd", "vignettes/nn2poly-03-data.R
 knitr::knit("vignettes/source/nn2poly-04-tests.Rmd", "vignettes/nn2poly-04-tests.Rmd")
 
 # move figures under the vignettes dir
-file.rename("figure", "vignettes/figure")
+file.copy("figure", "vignettes", recursive=TRUE)
+unlink("figure", recursive=TRUE, force=TRUE)
