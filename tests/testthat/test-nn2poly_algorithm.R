33b2b1f1fe89df6b6fac1ea709b20e82e0d97040
Iñaki Úcar <iucar@fedoraproject.org>
2022-06-17 14:36:26 +0200

    add test guard

--- tests/testthat/test-nn2poly_algorithm.R
+++ tests/testthat/test-nn2poly_algorithm.R
@@ -1,28 +1,26 @@
-test_that("Check that the algortihm provides a correct value for a certain
-          coefficient from a given example that has been computed manually", {
+test_that("Check algorithm against precomputed example", {
+  if (!reticulate::py_module_available("sympy"))
+    skip("sympy not available for testing")
 
-            # Load the example:
-            nn2poly_example <- nn2poly_example0
+  # Load the example:
+  nn2poly_example <- nn2poly_example0
 
-            # Get the needed data
-            weights_list <- nn2poly_example$weights_list
-            af_string_list <- nn2poly_example$af_string_list
-            q_taylor_vector <- nn2poly_example$q_taylor_vector
+  # Get the needed data
+  weights_list <- nn2poly_example$weights_list
+  af_string_list <- nn2poly_example$af_string_list
+  q_taylor_vector <- nn2poly_example$q_taylor_vector
 
-            result <- nn2poly_algorithm(
-              weights_list = weights_list,
-              af_string_list = af_string_list,
-              q_taylor_vector = q_taylor_vector,
-              historical_coeffs = TRUE
-            )
+  result <- nn2poly_algorithm(
+    weights_list = weights_list,
+    af_string_list = af_string_list,
+    q_taylor_vector = q_taylor_vector,
+    historical_coeffs = TRUE
+  )
 
-
-            # Desired coeffcient is  output y at layer 2, neuron 1,
-            # coefficient "1,1"
-            label <- result[[4]][[1]][[4]]
-            coeff <- result[[4]][[2]][4]
-            expect_equal(label,c(1,1))
-            expect_equal(coeff,0.63351833)
-
-
+  # Desired coeffcient is  output y at layer 2, neuron 1,
+  # coefficient "1,1"
+  label <- result[[4]][[1]][[4]]
+  coeff <- result[[4]][[2]][4]
+  expect_equal(label,c(1,1))
+  expect_equal(coeff,0.63351833)
 })
