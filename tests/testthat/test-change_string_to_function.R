998303842bc95b7d684038620f0f81c3c2e6bd20
nachodieez <nachodiez10@live.com>
2023-04-24 19:32:24 +0200

    Remove testthat:: from test calls.

--- tests/testthat/test-change_string_to_function.R
+++ tests/testthat/test-change_string_to_function.R
@@ -6,25 +6,25 @@ test_that("tests that the function to which it has been transformed is correct",
   af_function_list <- change_string_to_function(af_string_list)
 
   # softplus
-  testthat::expect_equal(
+  expect_equal(
     deparse(af_function_list[[1]]),
     deparse(function(x) log(1 + exp(x)))
   )
 
   # tanh
-  testthat::expect_equal(
+  expect_equal(
     deparse(af_function_list[[2]]),
     deparse(function(x) tanh(x))
   )
 
   # sigmoid
-  testthat::expect_equal(
+  expect_equal(
     deparse(af_function_list[[3]]),
     deparse(function(x) 1 / (1 + exp(-x)))
   )
 
   # linear
-  testthat::expect_equal(
+  expect_equal(
     deparse(af_function_list[[4]]),
     deparse(function(x) x)
   )
@@ -44,25 +44,25 @@ test_that("test that the value of  each function is the expected for
   random_numbers <- runif(1000, -1, 1)
 
   # softplus
-  testthat::expect_equal(
+  expect_equal(
     af_function_list[[1]](random_numbers),
     sapply(random_numbers, function(x) log(1 + exp(x)))
   )
 
   # tanh
-  testthat::expect_equal(
+  expect_equal(
     af_function_list[[2]](random_numbers),
     sapply(random_numbers, function(x) tanh(x))
   )
 
   # sigmoid
-  testthat::expect_equal(
+  expect_equal(
     af_function_list[[3]](random_numbers),
     sapply(random_numbers, function(x) 1 / (1 + exp(-x)))
   )
 
   # linear
-  testthat::expect_equal(
+  expect_equal(
     af_function_list[[4]](random_numbers),
     sapply(random_numbers, function(x) x)
   )
