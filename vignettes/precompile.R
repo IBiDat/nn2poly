f557ea82ffee66a60e5c28a0c808c871f6fd3255
moralapablo <moralapablo@gmail.com>
2023-06-19 19:17:53 +0200

    Updated vignettes 1 and 2. Removed vignettes 3 and 4 but left them in the source folder, as they contain the info needed to create some tests but not to be read by the user.

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,9 +1,10 @@
 # generate static Rmds
 knitr::knit("vignettes/source/nn2poly-01-introduction.Rmd.orig", "vignettes/nn2poly-01-introduction.Rmd")
 knitr::knit("vignettes/source/nn2poly-02-constraints.Rmd.orig", "vignettes/nn2poly-02-constraints.Rmd")
-knitr::knit("vignettes/source/nn2poly-03-data.Rmd.orig", "vignettes/nn2poly-03-data.Rmd")
-knitr::knit("vignettes/source/nn2poly-04-tests.Rmd.orig", "vignettes/nn2poly-04-tests.Rmd")
+# knitr::knit("vignettes/source/nn2poly-03-data.Rmd.orig", "vignettes/nn2poly-03-data.Rmd")
+# knitr::knit("vignettes/source/nn2poly-04-tests.Rmd.orig", "vignettes/nn2poly-04-tests.Rmd")
 
 # move figures under the vignettes dir
 file.copy("figure", "vignettes", recursive=TRUE)
 unlink("figure", recursive=TRUE, force=TRUE)
+
