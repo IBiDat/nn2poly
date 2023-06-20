6ecf83711aa371bbb49970c01de2c2485f6c2144
moralapablo <moralapablo@gmail.com>
2023-06-20 23:22:31 +0200

    wip

---
+++ R/plot_n_important_coeffs.R
@@ -0,0 +1,28 @@
+
+plot_n_important_coeffs <- function(poly, n_important_coeffs){
+
+  if (!requireNamespace("ggplot2", quietly = TRUE))
+    stop("package 'ggplot2' is required for this functionality", call.=FALSE)
+
+  if (!requireNamespace("cowplot", quietly = TRUE))
+    stop("package 'cowplot' is required for this functionality", call.=FALSE)
+
+  if (!requireNamespace("patchwork", quietly = TRUE))
+    stop("package 'patchwork' is required for this functionality", call.=FALSE)
+
+  M <- poly$values
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
