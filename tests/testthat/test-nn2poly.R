6004401c2cc32941d317cea894474d60c0fe0f10
Nacho Díez <119790922+nachodieez@users.noreply.github.com>
2023-06-21 18:32:59 +0200

    S3 (#32)
    This is work in progress towards implementing S3 classes and methods. Vignettes need to be updated to include all of this changes and will be used to verify its behavior.
    
    
    * Set `all_partitions` and `forced_max_Q` default values to NULL.
    
    The algorithm no longer checks if `all_partitions` and `forced_max_Q` are missing, but if their value is null.
    
    * S3 commit
    
    * new tests
    
    * extra

---
+++ tests/testthat/test-nn2poly.R
@@ -0,0 +1,67 @@
+test_that("nn2poly_algorithm:
+          Check algorithm against precomputed example", {
+  nn2poly_example <- nn2poly_example0
+
+  # Get the needed data
+  object <- nn2poly_example$weights_list
+  names(object) <- nn2poly_example$af_string_list
+
+  q_taylor_vector <- nn2poly_example$q_taylor_vector
+
+
+  result <- nn2poly(
+    object = object,
+    q_taylor_vector = q_taylor_vector,
+    store_coeffs = TRUE
+  )
+
+  # Output polynomial order is 4, as no order is forced and taylor
+  # vector is 2,2,1, so the product is 4:
+  n_terms <- length(result[[length(result)]]$labels)
+  order <- length(result[[length(result)]]$labels[[n_terms]])
+  expect_equal(order, 4)
+
+  # Desired coefficient is  output y at layer 2, neuron 1,
+  # coefficient "1,1"
+  label <- result[[4]]$labels[[4]]
+  coeff <- result[[4]]$values[1,4]
+  expect_equal(label,c(1,1))
+  expect_equal(coeff,0.63351833)
+})
+
+
+
+test_that("nn2poly_algorithm:
+          Check that the algortihm provides a correct value for a certain
+          coefficient from a given example that has been computed manually,
+          using the optional parameter `forced_max_Q`", {
+
+  # Load the example:
+  nn2poly_example <- nn2poly_example0
+
+  # Get the needed data
+  object <- nn2poly_example$weights_list
+  names(object) <- nn2poly_example$af_string_list
+  q_taylor_vector <- nn2poly_example$q_taylor_vector
+
+  result <- nn2poly(
+    object = object,
+    q_taylor_vector = q_taylor_vector,
+    store_coeffs = TRUE,
+    forced_max_Q = 2
+  )
+
+  # Output polynomial order is 2, as it is forced to be 2 instead of 4.
+  n_terms <- length(result[[length(result)]][[1]])
+  order <- length(result[[length(result)]][[1]][[n_terms]])
+  expect_equal(order, 2)
+
+  # Desired coeffcient is  output y at layer 2, neuron 1,
+  # coefficient "1,1"
+  label <- result[[4]][[1]][[4]]
+  coeff <- result[[4]]$values[1,4]
+  expect_equal(label,c(1,1))
+  expect_equal(coeff,0.63351833)
+
+
+})
