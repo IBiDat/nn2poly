03d64e6a286df5a5aaba1879921956cd0a6069c4
nachodieez <nachodiez10@live.com>
2023-11-08 12:41:35 +0100

    Modified `keras_constraint` function

--- R/keras_helpers.R
+++ R/keras_helpers.R
@@ -1,18 +1,27 @@
-keras_constraint <- function(type = c("l1_norm",
-                                      "l2_norm")) {
+#' keras weights and bias constrainer
+#'
+#' @param layer Layer of the keras model.
+#' @param type Constraint type.
+#'
+#' @return Keras array with constrained weight and bias
+#'
+#' @noRd
+keras_constraint <- function(layer,
+                             type = c("l1_norm", "l2_norm")) {
   type <- match.arg(type)
 
-  constraint <- if (type == "l1_norm") {
-    function(wb) {
-      norms   <- keras::k_sum(keras::k_abs(wb), axis = 1, keepdims = TRUE)
-      desired <- keras::k_clip(norms, 0, 1)
-      final   <- wb * (desired/(keras::k_epsilon() + norms))
+  w <- layer$get_weights()[[1]]
+  b <- layer$get_weights()[[2]]
+  wb <- rbind(b, w)
 
-      final
-    }
+  if (type == "l1_norm") {
+    norms   <- keras::k_sum(keras::k_abs(wb), axis = 1, keepdims = TRUE)
+    desired <- keras::k_clip(norms, 0, 1)
+    final   <- wb * (desired/(keras::k_epsilon() + norms))
   } else if (type == "l2_norm") {
-    keras::constraint_maxnorm(max_value = 1, axis = 0)
+    final <- keras::constraint_maxnorm(max_value = 1, axis = 0)(wb)
   }
 
-  constraint
+  keras::keras_array(list(final[2:NULL, ], final[1, ]))
+
 }
