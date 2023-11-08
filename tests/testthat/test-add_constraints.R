fe4d4011ea1df1d7b1286bdd251a5a8cfb906182
nachodieez <nachodiez10@live.com>
2023-11-08 12:46:46 +0100

    Minor changes

--- tests/testthat/test-add_constraints.R
+++ tests/testthat/test-add_constraints.R
@@ -17,33 +17,6 @@ test_that("Test that both the activation function and neurons are the same for
 
 })
 
-test_that("Test that the 'keep_old_weights' parameter works as intended", {
-  skip_if_not_installed("keras")
-  skip_if_not_installed("tensorflow")
-  skip_on_cran()
-
-  nn <- keras_test_model()
-
-  constrained_nn <- add_constraints(nn, keep_old_weights = TRUE)
-
-  nn_weights             <- get_parameters(nn)$weights_list
-  constrained_nn_weights <- get_parameters(constrained_nn)$weights_list
-
-  # check if the weights are the same
-  expect_identical(nn_weights, constrained_nn_weights)
-
-  # generate a new constrained nn without keeping the old weights
-  # (the function defaults `keep_old_weights = FALSE` but lets make it explicit)
-
-  constrained_nn2 <- add_constraints(nn, keep_old_weights = FALSE)
-
-  constrained_nn_weights2 <- get_parameters(constrained_nn2)$weights_list
-
-  # check if the weights are NOT the same
-  expect_false(identical(nn_weights, constrained_nn_weights2))
-
-})
-
 test_that("The function works over an already trained network and the connstraints
           are fulfiled after training for the constrained network", {
   skip_if_not_installed("keras")
@@ -61,32 +34,24 @@ test_that("The function works over an already trained network and the connstrain
                  optimizer = keras::optimizer_adam(),
                  metrics = "mse")
 
-  # train the model
-  keras::fit(nn,
-             x = testing_data$train_x,
-             y = testing_data$train_y,
-             verbose = 0,
-             epochs = 3,
-             validation_split = 0.2
-  )
-
   constrained_nn <- add_constraints(nn, constraint_type = "l1_norm")
 
+  # train the non-constrained model
+  fit(nn,
+      x = testing_data$train_x,
+      y = testing_data$train_y,
+      verbose = 0,
+      epochs = 3,
+      validation_split = 0.2
+  )
 
-
-  # compile the model
-  keras::compile(constrained_nn,
-                 loss = "mse",
-                 optimizer = keras::optimizer_adam(),
-                 metrics = "mse")
-
-  # train the model
-  keras::fit(constrained_nn,
-             testing_data$train_x,
-             testing_data$train_y,
-             verbose = 0,
-             epochs = 3,
-             validation_split = 0.2
+  # train the constrained model
+  fit(constrained_nn,
+      testing_data$train_x,
+      testing_data$train_y,
+      verbose = 0,
+      epochs = 3,
+      validation_split = 0.2
   )
 
   constrained_nn_parameters <- get_parameters(constrained_nn)
@@ -99,7 +64,7 @@ test_that("The function works over an already trained network and the connstrain
   # check if the condition is fulfilled for every matrix
   expect_true(
     all(
-      sapply(weights_l1_norm[1:length(weights_l1_norm)-1], # skip the output layer
+      sapply(weights_l1_norm[1:length(weights_l1_norm) - 1], # skip the output layer
              function(l1_norm) all(l1_norm < 1))
     )
   )
