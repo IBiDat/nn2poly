e865fdc4d1ea209e32d1bc9fe7150ddf5f8706d3
Iñaki Úcar <iucar@fedoraproject.org>
2023-11-14 14:54:03 +0100

    simplify add_constraints

--- R/add_constraints.R
+++ R/add_constraints.R
@@ -1,49 +1,32 @@
-# Functions needed to add the desired constraints by nn2poly to a given keras
-# model.
-
-# These constraints are expected to affect at the same time bias
-# and kernel weights, something that tensorflow does not support by default.
-# The idea is to bound the norm of the weight vector affecting arriving to each
-# neuron, including the bias of that neuron. The supported norms are L1 and L2,
-# but L1 is preferred, with 1 as it maximum norm bound.
-
-
-#' Add custom constraints to the weights of a keras neural network.
+#' Add constraints to a neural network
 #'
-#' This function is designed to transform a given keras NN (feed forward, dense),
-#' into a NN with the same structure, parameters and weights but with our custom
-#' layers that impose L1 and L2 norm constraints
+#' This function sets up a neural network object with the constraints required
+#' by the \code{\link{nn2poly}} algorithm.
 #'
-#' @param model A keras model to which the restrictions on the weights are to be applied.
-#' @param type The type of constraint you want to apply on the model. Currently,
-#' 'l1_norm' and 'l2_norm' can be applied.
+#' @param object A neural network object.
+#' @param type Constraint type. Currently, `l1_norm` and `l2_norm` are supported.
 #' @param ... Additional arguments (unused).
 #'
-#' @return A keras model with the custom constraints applied.
+#' @return A `nn2poly` neural network object.
 #'
 #' @export
-add_constraints <- function(model,
-                            type = c("l1_norm", "l2_norm"),
-                            ...) {
+add_constraints <- function(object, type = c("l1_norm", "l2_norm"), ...) {
   UseMethod("add_constraints")
 }
 
-#' @export
-add_constraints.keras.engine.training.Model <- function(model,
-                                                        type = c("l1_norm", "l2_norm"),
-                                                        ...) {
-
-  attr(model, "constraint") <- match.arg(type)
-  class(model) <- c("nn2poly", class(model))
-  model
+.add_constraints <- function(object, type = c("l1_norm", "l2_norm"), ...) {
+  attr(object, "constraint") <- match.arg(type)
+  class(object) <- c("nn2poly", class(object))
+  object
 }
 
 #' @export
-add_constraints.luz_module_generator <- function(model,
-                                                 type = c("l1_norm", "l2_norm"),
-                                                 ...) {
-  luz_model_sequential_check(model)
-  attr(model, "constraint") <- match.arg(type)
-  class(model) <- c("nn2poly", class(model))
-  model
+add_constraints.keras.engine.training.Model <- function(object, ...) {
+  .add_constraints(object, ...)
+}
+
+#' @export
+add_constraints.luz_module_generator <- function(object, ...) {
+  luz_model_sequential_check(object)
+  .add_constraints(object, ...)
 }
