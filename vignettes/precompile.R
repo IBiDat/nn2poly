63fb5fbe0cfe1a9815f4c39edd0468e2e1969dd0
moralapablo <moralapablo@gmail.com>
2023-12-11 01:32:11 +0100

    Updated and improved vignettes. New structure is 1-introduction, 2-DL frameworks and constraints, 3-classification example

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,8 +1,7 @@
 # generate static Rmds
 knitr::knit("vignettes/source/nn2poly-01-introduction.Rmd.orig", "vignettes/nn2poly-01-introduction.Rmd")
-knitr::knit("vignettes/source/nn2poly-02-tensorflow-regression.Rmd.orig", "vignettes/nn2poly-02-tensorflow-regression.Rmd")
-knitr::knit("vignettes/source/nn2poly-03-tensorflow-classification.Rmd.orig", "vignettes/nn2poly-03-tensorflow-classification.Rmd")
-knitr::knit("vignettes/source/nn2poly-04-torch-regression.Rmd.orig", "vignettes/nn2poly-04-torch-regression.Rmd")
+knitr::knit("vignettes/source/nn2poly-02-supported-DL-frameworks.Rmd.orig", "vignettes/nn2poly-02-supported-DL-frameworks.Rmd")
+knitr::knit("vignettes/source/nn2poly-03-classification-example.Rmd.orig", "vignettes/nn2poly-03-classification-example.Rmd")
 
 # move figures under the vignettes dir
 file.copy("figure", "vignettes", recursive=TRUE)
