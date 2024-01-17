9f462e3901a2a3839d0d29b963f9d94c5f3bffbf
Iñaki Úcar <iucar@fedoraproject.org>
2024-01-17 14:04:28 +0100

    rename vignette sources to prevent pkgdown from compiling them

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,9 +2,9 @@
 old_dir <- setwd("vignettes")
 
 # generate static Rmds
-knitr::knit("source/nn2poly-01-introduction.orig.Rmd", "nn2poly-01-introduction.Rmd")
-knitr::knit("source/nn2poly-02-supported-DL-frameworks.orig.Rmd", "nn2poly-02-supported-DL-frameworks.Rmd")
-knitr::knit("source/nn2poly-03-classification-example.orig.Rmd", "nn2poly-03-classification-example.Rmd")
+knitr::knit("source/_nn2poly-01-introduction.Rmd", "nn2poly-01-introduction.Rmd")
+knitr::knit("source/_nn2poly-02-supported-DL-frameworks.Rmd", "nn2poly-02-supported-DL-frameworks.Rmd")
+knitr::knit("source/_nn2poly-03-classification-example.Rmd", "nn2poly-03-classification-example.Rmd")
 
 # restore
 setwd(old_dir)
