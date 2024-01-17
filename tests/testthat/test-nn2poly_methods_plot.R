2fcb13e645c5f9cc91a76b630521dd0b84310dc7
moralapablo <moralapablo@gmail.com>
2024-01-17 15:31:05 +0100

    Added test for plot.nn2poly when n=NULL

--- tests/testthat/test-nn2poly_methods_plot.R
+++ tests/testthat/test-nn2poly_methods_plot.R
@@ -20,6 +20,27 @@ test_that("Test the plot for a polynomial generated with `keep_layers = FALSE`",
 })
 
 
+test_that("Test the plot for a polynomial generated with n = NULL", {
+  # loading the example
+  testing_data <- testing_helper_1()
+  object <- testing_data$weights_list
+  names(object) <- testing_data$af_string_list
+  taylor_orders <- testing_data$taylor_orders
+
+  # computing the polynomial
+  result <- nn2poly(
+    object = object,
+    taylor_orders = taylor_orders,
+    keep_layers = FALSE
+  )
+
+  # computing the plot with no limit for the important coefficients
+  p <- plot(result)
+
+  # testing the plot
+  vdiffr::expect_doppelganger("top NULL important coefficients", p)
+})
+
 
 test_that("Test the plot for a polynomial generated with  `keep_layers = TRUE`", {
   # loading the example
