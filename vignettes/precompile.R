547aa15030ed50f42e95bec680e31346173b433f
Iñaki Úcar <iucar@fedoraproject.org>
2024-01-17 13:23:23 +0100

    unexport plot_ functions

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,9 +1,10 @@
+# move to the vignettes directory
+old_dir <- setwd("vignettes")
+
 # generate static Rmds
-knitr::knit("vignettes/source/nn2poly-01-introduction.Rmd.orig", "vignettes/nn2poly-01-introduction.Rmd")
-knitr::knit("vignettes/source/nn2poly-02-supported-DL-frameworks.Rmd.orig", "vignettes/nn2poly-02-supported-DL-frameworks.Rmd")
-knitr::knit("vignettes/source/nn2poly-03-classification-example.Rmd.orig", "vignettes/nn2poly-03-classification-example.Rmd")
+knitr::knit("source/nn2poly-01-introduction.Rmd.orig", "nn2poly-01-introduction.Rmd")
+knitr::knit("source/nn2poly-02-supported-DL-frameworks.Rmd.orig", "nn2poly-02-supported-DL-frameworks.Rmd")
+knitr::knit("source/nn2poly-03-classification-example.Rmd.orig", "nn2poly-03-classification-example.Rmd")
 
-# move figures under the vignettes dir
-file.copy("includes", "vignettes", recursive=TRUE)
-unlink("includes", recursive=TRUE, force=TRUE)
-
+# restore
+setwd("old_dir")
