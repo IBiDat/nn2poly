bc736f1e12965882f4e870912129b702e546c968
Iñaki Úcar <iucar@fedoraproject.org>
2026-07-13 22:11:27 +0200

    minor tweaks

--- src/wrap.cpp
+++ src/wrap.cpp
@@ -12,34 +12,29 @@ List wrap(const PartitionsList& data) {
 List wrap(const WeightsList& data) {
   return List::create(
     _["labels"] = data.labels,
-    // Transpose to have polynomials as columns
     _["values"] = nn2poly::linalg::trans(data.values)
+    // Transpose to have polynomials as columns
   );
 }
 
 List wrap(const WeightsLists& data) {
   const int n_items = static_cast<int>(data.size());
-  const int n_layers = static_cast<int>(std::ceil(static_cast<double>(n_items) / 2.0));
+  const int n_layers = (n_items + 1) / 2;
 
   List out(n_layers);
   CharacterVector out_names(n_layers);
+  const CharacterVector layer_names = CharacterVector::create("input", "output");
 
   for (int i = 0; i < n_layers; i++) {
     const int input_index = 2 * i;
     const int output_index = 2 * i + 1;
 
     List layer(2);
-    CharacterVector layer_names = CharacterVector::create("input", "output");
-
     layer[0] = wrap(data[input_index]);
-    if (output_index < n_items) {
-      layer[1] = wrap(data[output_index]);
-    } else {
-      // If there is a linear output, i.e. single polynomial in final layer
-      // and odd number of items, then we repeat the input as the output, as
-      // the activation functions takes no effect on the polynomial
-      layer[1] = layer[0];
-    }
+    layer[1] = (output_index < n_items) ? wrap(data[output_index]) : layer[0];
+    // If there is a linear output, i.e. single polynomial in final layer
+    // and odd number of items, then we repeat the input as the output, as
+    // the activation functions takes no effect on the polynomial
 
     layer.attr("names") = layer_names;
     out[i] = layer;
